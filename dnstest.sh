4710be70d34d1401741baa11f4837ba5668d0af0
netscylla <github@netscylla.com>
2021-04-21 09:35:39 +0100

    added doh - kdig support cleaned up duplicate test servers

--- dnstest.sh
+++ dnstest.sh
@@ -2,7 +2,7 @@
 
 command -v bc > /dev/null || { echo "bc was not found. Please install bc."; exit 1; }
 { command -v drill > /dev/null && dig=drill; } || { command -v dig > /dev/null && dig=dig; } || { echo "dig was not found. Please install dnsutils."; exit 1; }
-
+{ command -v kdig > /dev/null && kdig=kdig; } || { echo "kdig was not found.Please install knot."; exit 1; }
 
 
 NAMESERVERS=`cat /etc/resolv.conf | grep ^nameserver | cut -d " " -f 2 | sed 's/\(.*\)/&#&/'`
@@ -22,8 +22,14 @@ PROVIDERS="
 8.26.56.26#comodo
 "
 
+DOH_PROVIDERS="
+1.1.1.1#cloudflare
+8.8.8.8#google
+9.9.9.9#quad9
+"
+
 # Domains to test. Duplicated domains are ok
-DOMAINS2TEST="www.google.com amazon.com facebook.com www.youtube.com www.reddit.com  wikipedia.org twitter.com gmail.com www.google.com whatsapp.com"
+DOMAINS2TEST="www.google.com amazon.com www.hotmail.com facebook.com www.reddit.com wikipedia.org "
 
 
 totaldomains=0
@@ -36,7 +42,9 @@ printf "%-8s" "Average"
 echo ""
 
 
-for p in $NAMESERVERS $PROVIDERS; do
+if [ -z "$1" ] 
+then
+  for p in $NAMESERVERS $PROVIDERS; do
     pip=${p%%#*}
     pname=${p##*#}
     ftime=0
@@ -55,9 +63,31 @@ for p in $NAMESERVERS $PROVIDERS; do
         ftime=$((ftime + ttime))
     done
     avg=`bc -lq <<< "scale=2; $ftime/$totaldomains"`
+    echo "  $avg"
+  done
+else
+  for p in $DOH_PROVIDERS; do
+    pip=${p%%#*}
+    pname=${p##*#}
+    ftime=0
+
+    printf "%-18s" "$pname"
+    for d in $DOMAINS2TEST; do
+        ttime=`$kdig +tls-ca +tls-host=$pip  +time=2 +stats @$pip $d |grep "Query time:" | cut -d : -f 2- | cut -d " " -f 2`
+        if [ -z "$ttime" ]; then
+            #let's have time out be 1s = 1000ms
+            ttime=1000
+        elif [ "x$ttime" = "x0" ]; then
+            ttime=1
+    fi
+
+        printf "%-8s" "$ttime ms"
+        ftime=$((ftime + ttime))
+    done
+    avg=`bc -lq <<< "scale=2; $ftime/$totaldomains"`
 
     echo "  $avg"
-done
-
+  done
+fi
 
 exit 0;
